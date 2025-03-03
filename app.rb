c32fed8eb12db941502ec189e4a10664eb774972
Bella Rose Wilson <bwilson9893@gmail.com>
2025-03-03 19:10:11 +0000

    routes added in the app

--- app.rb
+++ app.rb
@@ -7,3 +7,14 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  "This string will be sent as the body of the response"
+end
+
+get("/paper") do
+  "This string will be sent as the body of the response"
+end
+get("/scissors") do
+  "This string will be sent as the body of the response"
+end
