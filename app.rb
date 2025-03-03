8061cd79458cb506669f97cee8003a75e81f42a6
Bella Rose Wilson <bwilson9893@gmail.com>
2025-03-03 19:20:53 +0000

    copy for roots

--- app.rb
+++ app.rb
@@ -9,12 +9,48 @@ get("/") do
 end
 
 get("/rock") do
-  "This string will be sent as the body of the response"
+  "
+  <h2>  
+  We played Rock!
+  </h2>
+
+   <h2>  
+  They played Rock!
+  </h2>
+
+   <h2>  
+  We tied!
+  </h2>
+  "
 end
 
 get("/paper") do
-  "This string will be sent as the body of the response"
+  "
+  <h2>  
+  We played paper!
+  </h2>
+
+   <h2>  
+  They played paper!
+  </h2>
+
+   <h2>  
+  We tied!
+  </h2>
+  "
 end
 get("/scissors") do
-  "This string will be sent as the body of the response"
+  "
+  <h2>  
+  We played scissors!
+  </h2>
+
+   <h2>  
+  They played scissors!
+  </h2>
+
+   <h2>  
+  We scissors!
+  </h2>
+  "
 end
