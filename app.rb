7075861708bf076e2449bbdd2b20fd1462c95d2e
Bella Rose Wilson <bwilson9893@gmail.com>
2025-03-03 19:30:39 +0000

    array method added to rock

--- app.rb
+++ app.rb
@@ -9,13 +9,15 @@ get("/") do
 end
 
 get("/rock") do
+  moves = ["rock","paper","scissors"]
+  comp_move = moves.sample
   "
   <h2>  
   We played Rock!
   </h2>
 
    <h2>  
-  They played Rock!
+  They played "comp_move" +!
   </h2>
 
    <h2>  
