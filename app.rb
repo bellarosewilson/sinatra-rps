14baac6ecff3c30876c224bcdc1870f4f37be877
Bella Rose Wilson <bwilson9893@gmail.com>
2025-03-04 21:01:39 +0000

    rock, paper, scissors roots complete

--- app.rb
+++ app.rb
@@ -3,57 +3,35 @@ require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
+  <h1>Welcome to Rock-Paper-Scissors! </h1>
+  <p>From Wikipedia:</p>
   "
 end
 
 get("/rock") do
-  moves = ["rock","paper","scissors"]
+  moves = ["rock", "paper", "scissors"]
+  
   comp_move = moves.sample
+  
+  if comp_move == "rock"
+    outcome = "tied"
+  elseif  comp_move == "paper"
+    outcome = "lost"
+  else
+    outcome = "won"
+  end
 
   "
   <h2>  
-  We played Rock!
+    We played Rock!
   </h2>
 
    <h2>  
-  They played #{comp_move}+!
+    They played #{comp_move}!
   </h2>
 
    <h2>  
-  We tied!
-  </h2>
-  "
-end
-
-get("/paper") do
-  "
-  <h2>  
-  We played paper!
-  </h2>
-
-   <h2>  
-  They played #{comp_move}+!
-  </h2>
-
-   <h2>  
-  We tied!
-  </h2>
-  "
-end
-get("/scissors") do
-  "
-  <h2>  
-  We played scissors!
-  </h2>
-
-   <h2>  
-  They played #{comp_move}+!
-  </h2>
-
-   <h2>  
-  We scissors!
+    We #{outcome}!
   </h2>
   "
 end
